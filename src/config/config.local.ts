058aabecb7b55bc19080abe4bb8cf6b1fef18b07
damonxiong <xiongxiangquan@gmail.com>
2021-06-24 15:28:53 +0800

    add yml

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -7,11 +7,11 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: '123123',
-    database: 'cool',
+    username: 'dev',
+    password: 'WK5JbniQN',
+    database: 'mydb',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
     // 打印日志
