ff6f034496cd8764f5437a84e2dbbc094e902692
damonxiong <xiongxiangquan@gmail.com>
2021-06-25 11:18:33 +0800

    change db config

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -7,19 +7,11 @@ export default (appInfo: EggAppInfo) => {
 
   config.orm = {
     type: 'mysql',
-<<<<<<< HEAD
     host: 'localhost',
     port: 3306,
     username: 'dev',
     password: 'WK5JbniQN',
     database: 'mydb',
-=======
-    host: '10.211.55.7',
-    port: 3306,
-    username: 'midway',
-    password: '123123',
-    database: 'midway',
->>>>>>> 69188a93054b2427d00ef1f168323b3ad0f3af7f
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
     // 打印日志
