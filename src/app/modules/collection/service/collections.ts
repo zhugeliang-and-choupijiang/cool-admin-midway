063770b6cac2a059bdfca1923746adbbd6c723c2
damonxiong <xiongxiangquan@gmail.com>
2021-07-12 17:12:35 +0800

    axios add qs

--- src/app/modules/collection/service/collections.ts
+++ src/app/modules/collection/service/collections.ts
@@ -1,6 +1,7 @@
 import { Inject, Provide } from '@midwayjs/decorator';
 import { BaseService } from 'midwayjs-cool-core';
 import { ICoolCache } from 'midwayjs-cool-core';
+import axios from 'axios';
 
 /**
  * 商品
@@ -10,14 +11,19 @@ export class CollectionsService extends BaseService {
   @Inject('cool:cache')
   coolCache: ICoolCache;
 
+  qs = require('qs');
   /**
    * 返回所有数据
    */
   async collections(page: number, size: number) {
-    const axios = require('axios');
-    const json = await axios.request(
+    const json = await axios.get(
       'https://dev.ariginal.one/ariginalnfts/v1/collections',
-      { params: { page: page, limit: size } }
+      {
+        params: { page: page, limit: size },
+        paramsSerializer: params => {
+          return this.qs.stringify(params, { indices: false });
+        },
+      }
     );
     return json.data.data;
   }
